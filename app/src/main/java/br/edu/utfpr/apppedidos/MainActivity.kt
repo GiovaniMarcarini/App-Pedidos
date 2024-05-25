8d6eb3d48544edeae466f042cc0f1c5a77b2e448
GiovaniMarcarini <inf_giovani@hotmail.com>
2024-05-25 12:57:38 -0300

    UI Screen de Clientes

--- app/src/main/java/br/edu/utfpr/apppedidos/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/apppedidos/MainActivity.kt
@@ -3,6 +3,7 @@ package br.edu.utfpr.apppedidos
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,37 +11,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import br.edu.utfpr.apppedidos.ui.cliente.list.ClientesListScreen
 import br.edu.utfpr.apppedidos.ui.theme.AppPedidosTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             AppPedidosTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                ClientesListScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppPedidosTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/br/edu/utfpr/apppedidos/data/cliente/Endereco.kt
@@ -0,0 +1,11 @@
+package br.edu.utfpr.apppedidos.data.cliente
+
+class Endereco(
+
+    val cep: String = "",
+    val logradouro: String = "",
+    val numero: Int = 0,
+    val complemento: String = "",
+    val bairro: String = "",
+    val cidade: String = ""
+)
